934c8c53f8191fe97588b6822c603f3858cec9bc
Ycaro Rodrigues <112430708+ycarossauro@users.noreply.github.com>
2025-03-05 23:28:48 -0300

    feat: created folders for the chapters notes and new Exercises solution

---
+++ Codes/src/yourFirstProgramInKotlin/Chapter Exercises Solutions.kt
@@ -0,0 +1,45 @@
+package yourFirstProgramInKotlin
+
+private fun main() {
+    println("1")
+    println("2")
+    println("3")
+    println("------")
+    secondExercise()
+    println("------")
+    daysOfWeek()
+    println("------")
+    fourthExercise()
+    println("------")
+    fifthExercise()
+    println("------")
+    sixthExercise()
+}
+
+private fun secondExercise(){
+    println("I'm")
+    println("learning")
+    println("Kotlin!")
+}
+
+private fun daysOfWeek() {
+    println("Monday")
+    println("Wednesday")
+    println("Tuesday")
+    println("Thursday")
+    println("Friday")
+}
+
+private fun fourthExercise() {
+    println("Tomorrow is rainy")
+}
+
+private fun fifthExercise() {
+    println("There is a chance of snow")
+}
+
+private fun sixthExercise() {
+    println("Cloudy")
+    println("Partly Cloudy")
+    println("Windy")
+}
