45c8ba4ca207c2b6fa288b2bc741f9a7ad587736
Ycaro Rodrigues <112430708+ycarossauro@users.noreply.github.com>
2025-03-05 22:33:36 -0300

    feat: exercises in kotlin about function

--- Codes/src/yourFirstProgramInKotlin/Exercices of Functions.kt
+++ Codes/src/yourFirstProgramInKotlin/Exercices of Functions.kt
@@ -1,9 +1,13 @@
 package yourFirstProgramInKotlin
 
     fun main(){
-        greetUser("Ycaro")
+        val name = "Ycaro"
+        greetUser(name)
         println(calculateSum(5, 1))
         println(isEvenOrOdd(5))
+        println(checkNumber(0))
+        countDown(5)
+        greet(name, 26)
     }
 
 
@@ -22,6 +26,7 @@ package yourFirstProgramInKotlin
 /*
    2. Function with Inputs (Parameters)
     Modify this function so it returns the sum instead of printing it.
+    Code:
     fun calculateSum(a: Int, b: Int) {
     val sum = a + b
     println("The sum of $a and $b is: $sum")
@@ -31,6 +36,7 @@ package yourFirstProgramInKotlin
 /*
    3. Function with a Return Value
     Modify the function to print "Even" or "Odd" instead of returning a Boolean.
+    Code:
     fun isEven(number: Int): Boolean {
     return number % 2 == 0
 }
@@ -41,20 +47,61 @@ package yourFirstProgramInKotlin
     }
 
 /*
-   3. Function with a Return Value
-    Modify the function to print "Even" or "Odd" instead of returning a Boolean.
-    fun isEven(number: Int): Boolean {
-    return number % 2 == 0
+   4.  Using Kotlin Keywords Correctly
+   Modify the function so it returns a string instead of printing it.
+   Code:
+    fun checkNumber(num: Int) {
+    if (num > 0) {
+        println("Positive")
+    } else if (num < 0) {
+        println("Negative")
+    } else {
+        println("Zero")
+    }
 }
 */
-   private fun checkNumber(num: Int) {
-        if (num > 0) {
-            println("Positive")
+   private fun checkNumber(num: Int) : String {
+        return if (num > 0) {
+            "Positive"
         } else if (num < 0) {
-            println("Negative")
+            "Negative"
         } else {
-        println("Zero")
+        "Zero"
         }
+    }
+/*
+   5. Loop Inside a Function (Statements)
+    Modify the function to print "Blast Off!" after the countdown ends.
+   Code:
+    fun checkNumber(num: Int) {
+    if (num > 0) {
+        println("Positive")
+    } else if (num < 0) {
+        println("Negative")
+    } else {
+        println("Zero")
+    }
+}
+*/
+    private fun countDown(n: Int) {
+        for (i in n downTo 1) {
+            println(i)
+            if (i <= 1) println("Blast Off!")
+            }
+        }
+
+/*
+   6. Function with a Default Parameter
+    Modify this function to also take an age and print "Hello, Ycaro! You are 25 years old.".
+   Code:
+   fun greet(name: String = "Guest") {
+    println("Hello, $name!")
+}
+*/
+
+    private fun greet(name: String = "Guest", age: Int? = null) {
+        if(age == null) println("Hello, $name!")
+        else println("Hello, $name! You are $age years old.")
     }
 
 
